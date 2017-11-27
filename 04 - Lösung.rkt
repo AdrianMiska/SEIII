ae8fb0b0e97b31ae412b7444cf6b12ab09958bed
JanHottenrott <33556338+JanHottenrott@users.noreply.github.com>
2017-11-27 07:19:35 +0100

    Update 04 - Lösung.rkt

--- 04 - Lösung.rkt
+++ 04 - Lösung.rkt
@@ -1,31 +1,48 @@
 #lang racket
 ;;;Aufgabe 1
 ;1.) 6 , da max und min jeweils Funktionen sind und die Argumente daher am Anfang ausgewertet werden
-;2.) '(+ ,(- 13 11) 17) , ist das richtig so?
+;2.)
 ;3.) 'King , da cadr die Hintereinanderausführung von cdr car ist.
 ;4.) '() , da die zweimalige Anwendung von cdr genau '((On the feast of Steven)) liefert. Dies ist eine Liste mit nur einem Element, daher liefert die dritte Anwendung von cdr genau '()
 ;5.) '(When the snow lay round about) , da cons eine Funktion ist und daher 'When evaluiert wird und somit When der Liste hinzugefügt wird
 ;6.) '((Deep and) . crisp) , da das zweite Argument keine Liste ist und cons daher ein Paar ausgibt, bestehend aus der Liste '(Deep and) und crisp.
 ;7.) #t , da die beiden Listen dieselben Elemente beinhalten und equal? deshalb true ausgibt.
 ;8.) #f , da eq? überprüft, ob die Objekte identisch sind. Da beide Objekte separat voneinander erstellt werden, sind sie nicht identisch.
-(max (min 5 (- 8 7)) 6)
-'(+ ,(- 13 11) 17)
-(cadr '(Good King Wenceslas))
-(cdddr '(looked out (On the feast of Steven)))
-(cons 'When '(the snow lay round about))
-(cons '(Deep and) 'crisp)
-(equal? (list 'and 'even) '(and even))
-(eq? (list 'Rudolph 'the 'red-nosed 'reindeer)
-     (cons 'Rudolph '(the 'red-nosed 'reindeer)))
-
 
 ;;;Aufgabe 2
-(require racket/include)
-(include "phonetic.rkt")
+;Zur Übersicht führen wir die Funktion aus Aufgabenblatt 3, die wir hier benutzen wollen, noch einmal auf
+(define phoneticAlphabet (hash #\a "Alpha" #\b "Bravo"
+                               #\c "Charlie" #\d "Delta"
+                               #\e "Echo" #\f "Foxtrott"
+                               #\g "Golf" #\h "Hotel"
+                               #\i "India" #\j "Juliet"
+                               #\k "Kilo" #\l "Lima"
+                               #\m "Mike" #\n "November"
+                               #\o "Oscar" #\p "Papa"
+                               #\q "Quebec" #\r "Romeo"
+                               #\s "Sierra" #\t "Tango"
+                               #\u "Uniform" #\v "Viktor"
+                               #\w "Whiskey" #\x "X-Ray"
+                               #\y "Yankee" #\z "Zulu"
+                               #\0 "Nadazero" #\1 "Unoone"
+                               #\2 "Doutwo" #\3 "Terrathree"
+                               #\4 "Carrefour" #\5 "Pentafive"
+                               #\6 "Soxsix" #\7 "Setteseven"
+                               #\8 "Oktoeight" #\9 "Novonine"
+                               #\, "Decimal" #\. "Stop"
+                               #\space " "))
 
-;;1
+(define (string->phonetic s)
+  (letrec ([string->phoneticInner (λ (returnee charlist)
+                                    (if (null? charlist) returnee
+                                        (string->phoneticInner (cons (hash-ref phoneticAlphabet (char-downcase (car charlist)) void)
+                                                                     returnee)
+                                                               (cdr charlist))))])
+    (string->phoneticInner '() (reverse (string->list s)))))
 
-;;2
+;;Aufgabe 2.1
+
+;;Aufgabe 2.2
 
 ;Hilfsmethode zum Übersetzen des Formats aus Blatt 3
 (define (stringlist->string list)
@@ -47,7 +64,7 @@
                                 name " " (stringlist->string (string->phonetic rufzeichen)) "\n"
                                 "over")))
 
-;;3
+;;Aufgabe 2.3
 (display (notmeldung "Unicorn" "UCRN" "NOTFALLPOSITION UNGEFÄHR 5 SM NORDWESTLICH LEUCHTTURM ROTER SAND" "NOTFALLZEIT 1000 UTC \nSCHWERE SCHLAGSEITE WIR SINKEN \nKEINE VERLETZTEN \nSECHS MANN GEHEN IN DIE RETTUNGSINSEL \nSCHNELLE HILFE ERFORDERLICH \nICH SENDE DEN TRÄGER"))
 (display "\n\n")
 (display (notmeldung "Nautilus" "DEYJ" "NOTFALLPOSITION UNGEFÄHR 10 SM östlich Point Nemo 48° 52’ 31,75” S, 123° 23’ 33,07“ W" "Eine Riesenkrake hat das Schiff umschlungen \nGroßes Leck im Rumpf \n20 Personen an Bord \nTreiben antriebslos an Wasseroberfläche \nSende Peilzeichen"))
@@ -57,29 +74,29 @@
 
 
 ;;;Aufgabe 3
-;;1
+;;Aufgabe 3.1
 
 (define (hoch3 x) (* x x x))
 
 ;Innere Auswertung
-(hoch3 (+ 3 (hoch3 3)))
-(hoch3 (+ 3 (* 3 3 3)))
-(hoch3 (+ 3 27))
-(hoch3 30)
-27000
+;(hoch3 (+ 3 (hoch3 3)))
+;(hoch3 (+ 3 (* 3 3 3)))
+;(hoch3 (+ 3 27))
+;(hoch3 30)
+;27000
 
 ;Äußere Auswertung
-(hoch3 (+ 3 (hoch3 3)))
-(* (+ 3 (hoch3 3)) (+ 3 (hoch3 3)) (+ 3 (hoch3 3)))
-(* (+ 3 (* 3 3 3)) (+ 3 (* 3 3 3)) (+ 3 (* 3 3 3)))
-(* (+ 3 27) (+ 3 27) (+ 3 27))
-(* 30 30 30)
-27000
+;(hoch3 (+ 3 (hoch3 3)))
+;(* (+ 3 (hoch3 3)) (+ 3 (hoch3 3)) (+ 3 (hoch3 3)))
+;(* (+ 3 (* 3 3 3)) (+ 3 (* 3 3 3)) (+ 3 (* 3 3 3)))
+;(* (+ 3 27) (+ 3 27) (+ 3 27))
+;(* 30 30 30)
+;27000
 
-;;2
+;;Aufgabe 3.2
 ;Für normale Ausdrücke verwendet Racket die innere Auswertung, für S-Expressions die äußere Auswertung.
 
-;;3
+;;Aufgabe 3.3
 
 (define (new-if condition? then-clause else-clause)
   (cond (condition? then-clause)
@@ -89,7 +106,8 @@
   (new-if (> counter max-count)
           product
           (faculty (* counter product)
-                   (+ counter 1) max-count)))
+                   (+ counter 1)
+                   max-count)))
 
 ;Beim Ausführen von
 ;(faculty 1 1 5)
