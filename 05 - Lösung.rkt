ecb6256e106325e37e57c7d94b9eb50fcb32d74f
JanHottenrott <33556338+JanHottenrott@users.noreply.github.com>
2017-12-03 12:14:57 +0100

    Update 05 - Lösung.rkt

--- 05 - Lösung.rkt
+++ 05 - Lösung.rkt
@@ -5,7 +5,7 @@
 (define Musterung '(star dots stripes))
 (define Fühlerform '(curved curly straight))
 (define Flügelform '(ellipse rhomb hexagon))
-;Hilfsfunktion von Seite 145 des Skripts
+;Hilfsfunktion von Seite 145 des Skripts, die überprüft, ob ein Element in einer Liste enthalten ist
 (define (member? item xs)
   (if (null? xs)
       #f
@@ -24,7 +24,18 @@
 ;Funktion, die #t genau dann ausgibt, wenn das Merkmal A rezessiv gegenüber dem Merkmal B ist
 (define (rezessiv? A B)
   (member? A (rezessiveMerkmale B)))
-;Datenstruktur butterfly
+;Funktion, die bei Eingabe zweier Merkmale das dominante der beiden Merkmale ausgibt
+(define (dominantesMerkmal A B)
+  (if (dominant? A B)
+      A
+      B))
+;Funktion, die bei Eingabe zweier Merkmale das rezessive der beiden Merkmale ausgibt
+(define (rezessivesMerkmal A B)
+  (if (dominant? A B)
+      B
+      A))
+;Datenstruktur butterfly. Hierbei sollen sichtbar und unsichtbar zwei Listen sein, die jeweils ein Merkmal
+;jedes Merkmaltyps enthalten
 (define-struct butterfly (sichtbar unsichtbar))
 ;Funktion, die einen Schmetterling anhand der eingegebenen sichtbaren Merkmale mit zufällig rezessiven Merkmalen erstellt
 (define (createButterfly Farbe Muster Fühler Flügel)
@@ -49,3 +60,71 @@
   (if (butterfly? A)
       (butterfly-unsichtbar A)
       "gibDominant braucht einen Schmetterling als Eingabe"))
+;Funktion, die einen Schmetterling nach seinen dominanten Merkmalen zeichnet
+(define (zeichne A)
+  (let ([sichtbar (gibDominant A)])
+    (show-butterfly (car sichtbar) (cadr sichtbar) (caddr sichtbar) (cadddr sichtbar))))
+;Hilfsfunktion: Die Eingabe soll eine Liste sein, die aus 8 Merkmalen besteht, wobei zu jedem Merkmaltyp genau zwei Merkmale
+;enthalten sein sollen, die in der Liste bereits nebeneinander stehen. Die Hilfsfunktion sortiert die liste nun so um,
+;dass jeweils das dominante Merkmal links neben dem zugehörigen rezessiven Merkmal steht.
+(define (Hilfsfunktion1 xs)
+  (letrec ([HilfsfunktionInner (lambda (k ys acc)
+                                 (if (< 0 k)
+                                     (HilfsfunktionInner
+                                      (- k 1)
+                                      ys
+                                      (cons
+                                       (dominantesMerkmal (list-ref ys (* 2 k)) (list-ref ys (+ 1 (* 2 k))))
+                                       (cons
+                                        (rezessivesMerkmal (list-ref ys (* 2 k)) (list-ref ys (+ 1 (* 2 k))))
+                                        acc)))
+                                     (cons
+                                       (dominantesMerkmal (list-ref ys (* 2 k)) (list-ref ys (+ 1 (* 2 k))))
+                                       (cons
+                                        (rezessivesMerkmal (list-ref ys (* 2 k)) (list-ref ys (+ 1 (* 2 k))))
+                                        acc))))])
+    (HilfsfunktionInner 3 xs '())))
+;Eine weitere Hilfsfunktion: Die Eingabe soll wieder eine Liste sein, die dieselben Anforderungen erfüllt, wie die Eingabeliste
+;der ersten Hilfsfunktion. Die Liste wird nun mithilfe von Hilfsfunktion1 umsortiert und danach wird der dazugehörige Schmetterling
+;erstellt
+(define (Hilfsfunktion2 xs)
+  (let ([ys (Hilfsfunktion1 xs)])
+    (make-butterfly
+     (list
+      (list-ref ys 0)
+      (list-ref ys 2)
+      (list-ref ys 4)
+      (list-ref ys 6))
+     (list
+      (list-ref ys 1)
+      (list-ref ys 3)
+      (list-ref ys 5)
+      (list-ref ys 7)))))
+;Funktion, die bei Eingabe zweier Schmetterlinge ein Kind generiert. Hierbei wird zuerst eine Liste erstellt, die die zufällig
+;ausgewählten Merkmale der Elternteile enthält. Mit Hilfsfunktio2 wird dann der dazugehörende Schmetterling generiert.
+(define (generiereKind V M)
+  (letrec ([Inner (lambda (k A B C D acc)
+                    (if (< 0 k)
+                        (Inner
+                         (- k 1)
+                         A B C D 
+                         (cons
+                          (list-ref (list (list-ref A k) (list-ref B k)) (random 2))
+                          (cons
+                           (list-ref (list (list-ref C k) (list-ref D k)) (random 2))
+                           acc)))
+                         (cons
+                          (list-ref (list (list-ref A k) (list-ref B k)) (random 2))
+                          (cons
+                           (list-ref (list (list-ref C k) (list-ref D k)) (random 2))
+                           acc))))])
+    (Hilfsfunktion2 (Inner 3 (gibDominant V) (gibRezessiv V) (gibDominant M) (gibRezessiv M) '()))))
+;Funktion, die bei Eingabe zweier Schmetterlinge und einer natürlichen Zahl, die mindestens 1 ist, entsprechend
+;viele Kinder generiert und diese direkt zeichnet
+(define (generiereKinder V M n)
+  (letrec ([Inner (lambda (V M k acc)
+                    (if (< 1 k)
+                        (Inner
+                         V M (- k 1) (cons (generiereKind V M) acc))
+                        (cons (generiereKind V M) acc)))])
+    (map zeichne (Inner V M n '()))))
