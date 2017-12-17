bbf8e586f81f552f61dbf702562066bf64eefb6e
AdrianMiska <adrian.miska97@gmail.com>
2017-12-17 17:03:43 +0100

    1.1 und 1.2

---
+++ 07 - Lösung.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(require racket/trace)
+
+;;;Aufgabe 1
+
+(define (zaehlen1 x xs)
+  (if (null? xs)
+      0
+      (if (eq?(car xs) x)
+          (+ 1 (zaehlen1 x (cdr xs)))
+          (zaehlen1  x (cdr xs)))))
+
+(define (zaehlen2 x xs)
+  (letrec ([zaehlenInner (λ (x xs acc)
+                           (if (null? xs)
+                               acc
+                               (begin
+                                 (if (eq? (car xs) x)
+                                     (zaehlenInner x (cdr xs) (+ 1 acc))
+                                     (zaehlenInner x (cdr xs) acc)))))])
+    (zaehlenInner x xs 0)))
+
+
+(zaehlen1 3 '(2 4 3 2 3 4 1))
+(zaehlen2 3 '(2 4 3 2 3 4 1))
+(zaehlen3 3 '(2 4 3 2 3 4 1))
+
