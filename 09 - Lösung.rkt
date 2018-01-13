3881d3a3e6f8f4da38e455974f4283fdd66ee2a1
JanHottenrott <33556338+JanHottenrott@users.noreply.github.com>
2018-01-13 16:29:29 +0100

    Rename 08 - Lösung.rkt to 09 - Lösung.rkt

---
+++ 09 - Lösung.rkt
@@ -0,0 +1,57 @@
+#lang swindle
+(require swindle/setf
+swindle/misc )
+;;;Aufgabe 1.1
+;Definition der Klasse "Literaturbeitrag"
+(defclass* Literaturbeitrag ()
+  (Schlüssel :initarg :key
+             :reader key)
+  (Autor :initarg :autor
+         :reader autor)
+  (Erscheinungsjahr :initarg :year
+                    :reader year)
+  (Titel :initarg :titel
+         :reader titel)
+  :printer #t)
+;Definition der Klasse "Buch"
+(defclass* Buch (Literaturbeitrag)
+  (Verlag :initarg :verlag
+          :reader verlag)
+  (Verlagsort :initarg :place
+              :reader place)
+  (Reihe :initarg :series
+         :reader series)
+  (Seriennummer :Initarg :seriennummer
+                :reader seriennummer)
+  :printer #t)
+;Definition der Klasse "Sammelband"
+(defclass* Sammelband (Buch)
+  (Herausgeber :initarg :publisher
+               :reader publisher)
+  (Seitenangaben :initarg :pages
+                 :reader pages))
+;Definition der Klasse "Zeitschriftenartikel
+(defclass* Zeitschriftenartikel (Literaturbeitrag)
+  (Zeitschrift :initarg :magazine
+               :reader magazine)
+  (Bandnummer :initarg :bandnummer
+              :reader bandnummer)
+  (Heftnummer :initarg :heftnummer
+              :reader heftnummer)
+  (Erscheinungsmonat :initarg :month
+                     :reader month))
+;Beispielobjekte
+(define Beitrag1 (make Buch
+                       :autor 'Nessie
+                       :year 1790
+                       :titel "Mein Leben im Loch Ness: Verfolgt als Ungeheuer"
+                       :series "Die besondere Biographie"
+                       :seriennummer 1
+                       :verlag 'Iverness))
+(define Beitrag2 (make Zeitschriftenartikel
+                       :autor "H. G. Wells"
+                       :year 3200
+                       :titel "Zeitmaschinen leicht gemacht"
+                       :magazine "Heimwerkerpraxis für Anfänger"
+                       :bandnummer 500
+                       :heftnummer 3))
