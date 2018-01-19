abdce3c4a2b084302f1bd937090fe7ce5c879824
AdrianMiska <adrian.miska97@gmail.com>
2018-01-19 08:49:31 +0100

    Aufgabe 2.1

--- 10 - Lösung.rkt
+++ 10 - Lösung.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;;; Aufgabe 1
+;;; Aufgabe 1.1
 
 (define spiel #(0 0 0 0 0 9 0 7 0
                   0 0 0 0 8 2 0 5 0
@@ -28,7 +28,7 @@
 (define spiel->einträge (λ (spiel indizes)
                           (map (curry vector-ref spiel) indizes)))
 
-(define 0bis9 '(0 1 2 3 4 5 6 7 8))
+(define 0bis8 '(0 1 2 3 4 5 6 7 8))
 
 (define highest-number (λ (xs)
                          (let ([max (λ (x y) (if (> x y) x y))])
@@ -52,9 +52,9 @@
 
 (define spiel->konsistent? (λ (spiel)
                              (let(
-                                  [zeilen (map (curry spiel->einträge spiel) (map zeile->indizes 0bis9))]
-                                  [spalten (map (curry spiel->einträge spiel) (map spalte->indizes 0bis9))]
-                                  [quadranten (map (curry spiel->einträge spiel) (map quadrant->indizes 0bis9))])
+                                  [zeilen (map (curry spiel->einträge spiel) (map zeile->indizes 0bis8))]
+                                  [spalten (map (curry spiel->einträge spiel) (map spalte->indizes 0bis8))]
+                                  [quadranten (map (curry spiel->einträge spiel) (map quadrant->indizes 0bis8))])
                                
                                (and
                                 (all-true (map liste->konsistent? zeilen))
@@ -62,10 +62,39 @@
                                 (all-true (map liste->konsistent? quadranten)))))) ;alle konsistent
 
 (define spiel->gelöst? (λ (spiel)
-                         (let([zeilen (map (curry spiel->einträge spiel) (map zeile->indizes 0bis9))]
-                              [spalten (map (curry spiel->einträge spiel) (map spalte->indizes 0bis9))]
-                              [quadranten (map (curry spiel->einträge spiel) (map quadrant->indizes 0bis9))])
+                         (let([zeilen (map (curry spiel->einträge spiel) (map zeile->indizes 0bis8))]
+                              [spalten (map (curry spiel->einträge spiel) (map spalte->indizes 0bis8))]
+                              [quadranten (map (curry spiel->einträge spiel) (map quadrant->indizes 0bis8))])
                            (and
                             (all-true (map liste->gelöst? zeilen))
                             (all-true (map liste->gelöst? spalten))
                             (all-true (map liste->gelöst? quadranten)))))) ;alle gelöst
+
+;Aufgabe 1.2
+
+(define index->quadrant (λ (x) (+ (* 3 (floor (/ (index->spalte x) 3)))
+                                  (floor (/ (modulo (index->zeile x) 9) 3)))))
+(define index->spalte (λ (x) (floor(/ x 9))))
+(define index->zeile (λ (x) (modulo x 9)))
+
+(define zero-indices (λ (xs)
+                       (for/list ([i xs]
+                                  [n (in-naturals)]
+                                  #:when (zero? i))
+                         n)))
+
+(define zahl-möglich (λ (spiel zahl index)
+                       (let ([zeile (spiel->einträge spiel (zeile->indizes(index->zeile index)))]
+                             [spalte (spiel->einträge spiel (spalte->indizes (index->spalte index)))]
+                             [quadrant (spiel->einträge spiel (quadrant->indizes (index->quadrant index)))])
+                         (and
+                          (liste->konsistent? (cons zahl zeile))
+                          (liste->konsistent? (cons zahl spalte))
+                          (liste->konsistent? (cons zahl quadrant)))))) ;zahl möglich = passt in alle noch rein
+
+
+(define markiere-ausschluss (λ (spiel zahl)
+                              (let* ([returnee (vector-copy spiel)]
+                                     [0-indices (zero-indices spiel)]
+                                     [x-indices (filter (curry zahl-möglich spiel zahl) 0-indices)])
+                                "TODO make vector x at x-indices")))
